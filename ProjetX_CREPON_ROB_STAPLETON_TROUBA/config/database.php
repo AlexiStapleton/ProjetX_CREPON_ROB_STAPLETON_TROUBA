96bb1b136341b66625792741c3776f1253f27387
Pytokaa <elioth.rob@proton.me>
2025-03-28 15:28:39 +0100

    TOUJOURS PLUS DE PUSH

--- ProjetX_CREPON_ROB_STAPLETON_TROUBA/config/database.php
+++ ProjetX_CREPON_ROB_STAPLETON_TROUBA/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,16 +84,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            //'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '51.83.36.122'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'projetx_crepon_rob_stapleton_trouba'),
+            'username' => env('DB_USERNAME', 'staale'),
+            'password' => env('DB_PASSWORD', '2yn32i'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'twitter',
             'sslmode' => 'prefer',
         ],
 
