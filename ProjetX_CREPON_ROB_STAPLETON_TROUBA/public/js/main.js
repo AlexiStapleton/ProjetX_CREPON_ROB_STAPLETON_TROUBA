24a4ae132439d0c12b130d384a2a1f6dc5d6b32e
Mael <mael.trouba@etu.univ-smb.fr>
2025-04-01 09:54:50 +0200

    Correction de la page de connexion + css du input file dans le feed

--- ProjetX_CREPON_ROB_STAPLETON_TROUBA/public/js/main.js
+++ ProjetX_CREPON_ROB_STAPLETON_TROUBA/public/js/main.js
@@ -1,9 +1,9 @@
 const buttonConnexion = document.getElementById("b_connexion");
 const backgroundPopup = document.getElementById("background_popup");
 const buttonClose = document.getElementById("button_close");
-//const buttonInscription = document.getElementById("b_creer_compte")
-//const backgroundPopupInscription = document.getElementById("background_popup_inscription");
-//const buttonCloseInscription = document.getElementById("button_close_inscription");
+const buttonInscription = document.getElementById("b_creer_compte")
+const backgroundPopupInscription = document.getElementById("background_popup_inscription");
+const buttonCloseInscription = document.getElementById("button_close_inscription");
 
 buttonConnexion.addEventListener("click", () =>{
     backgroundPopup.style.display = "flex";
@@ -13,10 +13,10 @@ buttonClose.addEventListener("click", () =>{
     backgroundPopup.style.display = "none";
 });
 
-/*buttonInscription.addEventListener("click", () =>{
+buttonInscription.addEventListener("click", () =>{
     backgroundPopupInscription.style.display = "flex";
-});*/
+});
 
-/*buttonCloseInscription.addEventListener("click", () =>{
+buttonCloseInscription.addEventListener("click", () =>{
     backgroundPopupInscription.style.display = "none";
-});*/
+});
